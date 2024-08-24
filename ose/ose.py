86a49fc9bae2e0d8b54892e04c3601f2244b3e32
Zack Watts <zaxor0@gmail.com>
2024-08-24 17:51:08 -0400

    choicest classes picked per stts

--- ose/ose.py
+++ ose/ose.py
@@ -11,9 +11,16 @@ attr = { 'Strength' : 0, 'Intelligence' : 0, 'Wisdom' : 0, 'Dexterity' : 0, 'Con
 
 def main():
   # roll stats
+  highestRoll = 0
   for stat in attr:
     statRoll = diceRoll(3,6)
+    if statRoll > highestRoll:
+      highestRoll = statRoll
     attr[stat] = statRoll
+  bestStats = []
+  for stat in attr:
+    if attr[stat] >= highestRoll:
+      bestStats.append(stat)
   possibleClasses = []
   # create an array of classes the rolls qualify the player for
   for pClass in playerClasses:
@@ -32,7 +39,26 @@ def main():
           reqMeetCount += 1
       if reqMeetCount == reqCount:
         possibleClasses.append(pClass)
-  playerClass = random.choice(possibleClasses)
+  # narrow the choices to those whose prime req. matches the player's highest stat(s)
+  choicestClasses = []
+  for pClass in possibleClasses:
+    for pr in playerClasses[pClass]['Prime-Requisite']:
+      if pr in bestStats:      
+        choicestClasses.append(pClass)
+  if len(choicestClasses) == 0:
+    # if choicest classes is empty, can happen if con is your best score, compare against second best
+    nextHighestRoll = 0
+    for stat in attr:
+      if stat != bestStats[0] and attr[stat] > nextHighestRoll:
+        nextHighestRoll = attr[stat]
+        nextHighestStat = stat
+    choicestClasses = []
+    for pClass in possibleClasses:
+      for pr in playerClasses[pClass]['Prime-Requisite']:
+        if pr == nextHighestStat:      
+          choicestClasses.append(pClass)
+
+  playerClass = random.choice(choicestClasses)
   characterSheet(playerClass, attr)
 
 def characterSheet(playerClass, attr):
