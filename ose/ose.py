f9b081257291d772b16e93559ff322eff0207141
Zack Watts <zaxor0@gmail.com>
2024-08-24 16:26:24 -0400

    push

--- ose/ose.py
+++ ose/ose.py
@@ -7,16 +7,27 @@ with open('player-classes.yaml','r') as file:
   playerClasses = yaml.safe_load(file)
 
 
-attr = { 'strength' : 0, 'intelligence' : 0, 'wisdom' : 0, 'dexterity' : 0, 'constitution' : 0, 'charisma' : 0 }
+attr = { 'Strength' : 0, 'Intelligence' : 0, 'Wisdom' : 0, 'Dexterity' : 0, 'Constitution' : 0, 'Charisma' : 0 }
 
 def main():
   # roll stats
   for stat in attr:
     statRoll = diceRoll(3,6)
     attr[stat] = statRoll
-
   print(attr)
-
+  possibleClasses = []
+  # create an array of classes the rolls qualify the player for
+  for pClass in playerClasses:
+    qClass  = playerClasses[pClass]
+    # if there are no requirements, add it to the array
+    if qClass['Requirements'] == 'None':
+      possibleClasses.append(qClass)
+    # else evaluate player stats against class requirements
+    else:
+      reqs = qClass['Requirements']
+      for stat in reqs:
+        if attr[stat] >= reqs[stat]:
+          print(stat)
 
 def diceRoll(dieCount,dieSides):
   dieTotal = 0
