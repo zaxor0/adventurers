8745b6036168cec917a96fb290ccb20714b8bef5
Zack Watts <zaxor0@gmail.com>
2024-08-24 16:42:47 -0400

    adding printout

--- ose/ose.py
+++ ose/ose.py
@@ -14,20 +14,36 @@ def main():
   for stat in attr:
     statRoll = diceRoll(3,6)
     attr[stat] = statRoll
-  print(attr)
   possibleClasses = []
   # create an array of classes the rolls qualify the player for
   for pClass in playerClasses:
     qClass  = playerClasses[pClass]
     # if there are no requirements, add it to the array
     if qClass['Requirements'] == 'None':
-      possibleClasses.append(qClass)
+      possibleClasses.append(pClass)
     # else evaluate player stats against class requirements
     else:
       reqs = qClass['Requirements']
+      reqCount = 0
+      reqMeetCount = 0
       for stat in reqs:
+        reqCount += 1
         if attr[stat] >= reqs[stat]:
-          print(stat)
+          reqMeetCount += 1
+      if reqMeetCount == reqCount:
+        possibleClasses.append(pClass)
+  playerClass = random.choice(possibleClasses)
+  characterSheet(playerClass, attr)
+
+def characterSheet(playerClass, attr):
+  print('you are a', playerClass)
+  for stat in attr:
+    tabs = '\t'
+    if len(stat) < 7:
+      tabs = '\t\t'
+    print(stat,tabs,attr[stat])
+
+
 
 def diceRoll(dieCount,dieSides):
   dieTotal = 0
